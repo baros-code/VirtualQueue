58f0993b89a17572aa5feb091b8ad888cc9c9dac
Kaplan <63818036+Kaplan80@users.noreply.github.com>
2021-05-13 23:47:29 +0300

    Date scrollpicker added

--- src/Client/CreateReservation.js
+++ src/Client/CreateReservation.js
@@ -1,26 +1,145 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
-import ReservationForm from '../components/ReservationForm';
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, View,Text, TextInput,Button} from 'react-native';
 import { firebase } from '../firebase/config'
+import ScrollPickerComponent from "../ExternalComponents/ScrollPickerComponent"
+import { Feather } from '@expo/vector-icons'; 
 
 const CreateReservation = ({ navigation }) => {
 
-    const serviceType = navigation.getParam('serviceType');
     const organizationId = navigation.getParam('organizationId');       //selected organization to enqueue
-    const organizationName = navigation.getParam('organizationName');       
+    const organizationName=navigation.getParam('organizationName');     
     const clientId = navigation.getParam('clientId');
+    const [queues,setQueues]=useState([])
+    const[queueId,setQueueId]=useState("Choose transaction type")
+    const [dates, setDates] = useState([]);
+    const [times, setTimes] = useState([]);
+    const [date, setDate] = useState("Choose Date");
+    const [time, setTime] = useState("Choose Time");
+    const[transactionPicked,setTransactionPicked]=useState(false)
+    const[datePicked,setDatePicked]=useState(false)
 
-    console.log("From CreateReservation: " + clientId, organizationId, organizationName);
 
+    async function removeTimeFromQueue() {
+       let ref = await firebase.database().ref('queues/' + queueId + "/dates/" + date);
+       await ref.once("value",function (dateSnapShot) {
+            dateSnapShot.forEach( (dateAttribute) => {
+                let currentTime=dateAttribute.val()
+                if (currentTime === time) {
+                    let currentKey=dateAttribute.key
+                    ref.child(currentKey).remove()
+                }
+            });
+        });
+
+    }
+    
+    function biggerThanToday(date) {
+            let dateFormat=date.split("-")
+            let compareDate=new Date(dateFormat[2],dateFormat[1]-1,dateFormat[0]);
+            let today=new Date()
+            return (compareDate.getTime() >= today.getTime())
+    }
+
+
+    function getTransactionType() {
+        let type;
+        queues.forEach((queue) => {
+            if (queue.value === queueId) {
+                type=queue.transactionType
+            }
+        })
+        return type
+    }
+
+
+    useEffect(() => {
+
+        async function fetchDatesFromQueue() {
+            const queuesDateReference=await firebase.database().ref("queues/" + queueId + "/dates")
+                let dates=[]
+                await queuesDateReference.once("value",function (datesReference) {
+                    datesReference.forEach( dateSnapShot => {
+                        let currentDate={}
+                        currentDate.value=dateSnapShot.key
+                        currentDate.label=dateSnapShot.key
+                        if (biggerThanToday(currentDate.value)) {
+                            dates.push(currentDate)
+                        }
+                    });
+                   
+                })
+                setDates(dates)
+                setTransactionPicked(true)
+    
+        }
+    
+        async function fetchTimesFromDate() {
+            const timesReference=await firebase.database().ref("queues/" + queueId + "/dates/" + date)
+                let times=[]
+                await timesReference.once("value",function (timeReference) {
+                    let timesList=timeReference.val()
+                    timesList.forEach(time => {
+                        let currentTime={}
+                        console.log(time)
+                        currentTime.value=time
+                        currentTime.label=time
+                        times.push(currentTime)
+                    })
+                   
+                })
+                setTimes(times)
+                setDatePicked(true)
+    
+        }
+        
+        async function fetchOrganizationQueues() {
+            let queues=[]
+            const queuesReference=await firebase.database().ref("queues/")
+            await queuesReference.once("value",function (queuesReference) {
+                queuesReference.forEach( dateSnapShot => {
+                    let currentDay=dateSnapShot.val()
+                    currentDay.value=dateSnapShot.key
+                    currentDay.label=currentDay.transactionType
+                    let currentOrganizationId=currentDay.organizationId
+                    if (currentOrganizationId === organizationId) {
+                      queues.push(currentDay)                 
+                    }
+                });
+               
+            })
+           setQueues(queues)
+           
+        }
+
+
+
+        if (queueId === "Choose transaction type") {
+            fetchOrganizationQueues()
+        } else if ( date === "Choose Date") {
+            fetchDatesFromQueue()
+        } else if (time === "Choose Time"){
+            fetchTimesFromDate() 
+            
+        } else { 
+            setTime("Choose Time")                      
+            fetchDatesFromQueue()
+            fetchTimesFromDate() 
+        }
+        
+      },([queueId,date]))
+
+
+
+/** 
     const findQueue = async (transactionType) => {
         var ref = await firebase.database().ref("queues");   
         var response;
         await ref.once("value",function (queues) {
           queues.forEach( queue => {
-              let currentQueue = queue.val();
-              currentQueue.id = queue.key;
-              if(currentQueue.organizationId === organizationId && currentQueue.transactionType === transactionType) {
-                  response = currentQueue;
+              let currentDay = queue.val();
+              currentDay.id = queue.key;
+              if(currentDay.organizationId === organizationId && currentDay.transactionType === transactionType) {
+                  response = currentDay;
                   return true;      //end forEach
                   //takeValue(response);        herhangi bir methoda argüman olarak verirsek promise dönebiliyor, alternatif return şekli.
               }
@@ -28,37 +147,57 @@ const CreateReservation = ({ navigation }) => {
       }); 
       return response;
     }
-
-    const addReservation = async (clientId, transactionType, date, callback) => {
-
+*/
+    const addReservation = async () => {
+        await removeTimeFromQueue() 
+        let type=getTransactionType()
         var ref = await firebase.database().ref("reservations").push();      //push sayesinde unique key'li branch olarak ekliyor.
-        findQueue(transactionType).then(response => {
-            if(response) {
-                ref.set({
-                    date: date,
-                    clientId: clientId,
-                    employeeId: "",
-                    estimatedRemainingTimeSec: "300",
-                    organizationId: organizationId,
-                    organizationName: organizationName,
-                    queueId: response.id,
-                    status: 0,
-                    transactionType: transactionType
-                    });
-                callback(); //navigate
-            }else {
-                console.log("-----------------------------QUEUE NOT FOUND!------------------------------");
-            }
-        });
+        await ref.set({
+            date: date.split("-").join("/"),
+            time:time,
+            clientId: clientId,
+            employeeId: "",
+            estimatedRemainingTimeSec: "",
+            organizationId: organizationId,
+            organizationName: organizationName,
+            queueId: queueId,
+            status: 0,
+            transactionType : type
+            });
+        navigation.navigate('ClientDashboard')
+           
+        
     };
 
+
     return (
-        <ReservationForm
-        initialValues={{organizationName: organizationName, transactionType: ''}} 
-        onSubmit={(transactionType, date) => { addReservation(clientId, transactionType, date, () => navigation.navigate('ClientDashboard'))}}
-        type={serviceType}
-        />
+        <View>
+        <Text style={styles.label}>Organization Name:</Text>
+        <TextInput editable={false} style={styles.input} value={organizationName} onChangeText={(text) => setOrganizationName(text)} />
+        <Text style={styles.label}>Transaction Type:</Text>
+        <ScrollPickerComponent style={styles.input}  data={queues} editable={true} selectedValue={queueId} callback={ (value) => setQueueId(value)} />
+        
+        <View style={styles.datePicker}>   
+        {transactionPicked &&
+        <View style={styles.pickerStyle}>
+        <Feather style={styles.iconStyle} name="calendar" />
+        <ScrollPickerComponent style={styles.input2}  data={dates} editable={true} selectedValue={date} callback={  (value) => setDate(value)} />
+        </View>
+        }
+        {datePicked &&
+         <View style={styles.pickerStyle}>
+         <Feather style={styles.iconStyle} name="clock" />
+        <ScrollPickerComponent style={styles.input2}  data={times} editable={true} selectedValue={time} callback={(value) => setTime(value)} />
+        </View> 
+        }
+        </View>
+        <View style={styles.button}>
+            <Button  title="Save Reservation" onPress={() => addReservation() } />
+        </View>
+    </View>
     );
+
+        
 };
 
 
@@ -68,7 +207,53 @@ CreateReservation.navigationOptions = ( ) => {
     };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {    
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+        color: 'white'
+    },
+    input2: {    
+        fontSize: 20,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius:10,
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+        color: 'white'
+    },
+    iconStyle: {
+        fontSize: 40,
+        margin: 15,
+        color: 'white'
+    },
+    label: {
+        padding:5,
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5,
+        color: 'white'
+    },
+    pickerStyle: {
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    button: {
+        alignSelf: 'flex-end',
+        marginRight: 10,
+        paddingTop: 70
+    },
+    datePicker: {
+        paddingHorizontal:47,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
 
+})
 
 export default CreateReservation;
